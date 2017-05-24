19a4ba0d5e55e11f5ee1dee645729eb047bcf7fa
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-05-24 14:30:16 +0100

    Basic token test complete

--- contracts/MakersToken.sol
+++ contracts/MakersToken.sol
@@ -2,15 +2,28 @@ pragma solidity ^0.4.8;
 
 contract MakersToken {
 
+  string public name;
+  string public symbol;
+  uint8 public decimals;
+
   mapping (address => uint256) public BalanceOf;
 
-  function MakersToken(uint256 initialSupply) {
+  event Transfer(address indexed from, address indexed to, uint256 value);
+
+  function MakersToken(uint256 initialSupply, string tokenName, uint8 decimalUnits, string tokenSymbol) {
     BalanceOf[msg.sender] = initialSupply;
+    name = tokenName;
+    symbol = tokenSymbol;
+    decimals = decimalUnits;
   }
 
   function transfer(address _to, uint256 _value) {
+    if (BalanceOf[msg.sender] < _value || BalanceOf[_to] + _value < BalanceOf[_to])
+      throw;
+
     BalanceOf[msg.sender] -= _value;
     BalanceOf[_to] += _value;
+    Transfer(msg.sender, _to, _value);
   }
 
   function getBalance() returns (uint) {
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var MakersToken = artifacts.require("./MakersToken.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(MakersToken);
+  deployer.deploy(MakersToken, 100, "MakersToken", 2, "M");
 };
