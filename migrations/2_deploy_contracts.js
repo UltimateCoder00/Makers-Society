776948cd8f1cbe1b77edb29cfd2a471012c79e75
Stephanie Crampin <steph.crampin@gmail.com>
2017-05-26 11:32:56 +0100

    extracted owner contract and passed all tests

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
+var Owned = artifacts.require("./Owned.sol");
 var MakersToken = artifacts.require("./MakersToken.sol");
 var Congress = artifacts.require("./Congress.sol");
 
 module.exports = function(deployer) {
+  deployer.deploy(Owned);
   deployer.deploy(MakersToken, 100, "MakersToken", 2, "M",0);
   deployer.deploy(Congress, 100, 200, 300);
 };
