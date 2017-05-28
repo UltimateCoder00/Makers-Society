e1a18b2a8b3ae19915752acd82b7a55b14edbcef
Edward Ward <edwardwardward@gmail.com>
2017-05-28 21:30:40 +0100

    attempt to add transaction logs - WIP

--- src/components/PointsDisplay.js
+++ src/components/PointsDisplay.js
@@ -12,7 +12,8 @@ export default class PointsDisplay extends React.Component {
   constructor() {
     super();
     this.state = {
-      accountPoints: 'loading points...'
+      accountPoints: 'loading points...',
+      transactionHistory: 'loading points...'
     };
   }
 
@@ -24,15 +25,9 @@ export default class PointsDisplay extends React.Component {
     const makersToken = contract(MakersTokenContract)
     makersToken.setProvider(provider)
 
-    // Get Web3 so we can get our accounts.
     const web3RPC = new Web3(provider)
 
-    // Declaring this for later so we can chain functions on MakersToken.
     var makersTokenInstance
-
-    // // Get accounts.
-    // web3RPC.eth.getAccounts(function(error, accounts) {
-    //   // console.log(accounts)
 
       makersToken.deployed().then(function(instance) {
         makersTokenInstance = instance;
@@ -41,13 +36,28 @@ export default class PointsDisplay extends React.Component {
         return self.setState({ accountPoints: result.c[0] })
       })
     // })
-  }
+
+    makersToken.deployed().then(function(instance) {
+      makersTokenInstance = instance;
+      debugger;
+      var transferEventAll = makersTokenInstance.Tranfser({_sender: '0x6630A2Af9f49c14C6beDeac703eb56316bd0e950'}, {fromBlock: 0, toBlock: 'latest'});
+      transferEventAll.watch(function(err, result) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log(result.args);
+      });
+    }
+  )};
+
 
   render() {
     this.loadPoints();
     return (
       <div>
         <h1>You have {this.state.accountPoints} MakerPoints!</h1>
+        <h1>You have {this.state.transactionHistory} MakerPoints!</h1>
       </div>
     );
   }
