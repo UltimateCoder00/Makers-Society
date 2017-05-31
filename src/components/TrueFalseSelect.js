97dbce5976fb38133c87005739f68473faf20fc8
Edward Ward <edwardwardward@gmail.com>
2017-05-31 10:05:00 +0100

    Delete Hashed out lines

--- src/components/TrueFalseSelect.js
+++ src/components/TrueFalseSelect.js
@@ -10,23 +10,6 @@ import '../css/pure-min.css'
 import '../App.css'
 
 export default class TrueFalseSelect extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {value: 'coconut'};
-  //
-  //   this.handleChange = this.handleChange.bind(this);
-  //   this.handleSubmit = this.handleSubmit.bind(this);
-  // }
-  //
-  // handleChange(event) {
-  //   this.setState({value: event.target.value});
-  // }
-  //
-  // handleSubmit(event) {
-  //   alert('Your favorite flavor is: ' + this.state.value);
-  //   event.preventDefault();
-  // }
-  // value={this.state.value}
 
   render() {
     return (
